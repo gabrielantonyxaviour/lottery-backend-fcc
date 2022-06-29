e9012cf6be1ba470a5dad6370c431b33c9faf60e
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2022-06-29 23:06:23 +0530

    clean up

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,23 +10,9 @@ require("dotenv").config()
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-// const MAINNET_RPC_URL =
-//     process.env.MAINNET_RPC_URL ||
-//     process.env.ALCHEMY_MAINNET_RPC_URL ||
-//     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-// const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
-// const POLYGON_MAINNET_RPC_URL =
-//     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
-// // optional
-// const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
-
-// // Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
-// const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
-// const REPORT_GAS = process.env.REPORT_GAS || false
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
     solidity: "0.8.7",
     defaultNetwork: "hardhat",
@@ -37,6 +23,11 @@ module.exports = {
         noColors: true,
         coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
+    etherscan: {
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
+    },
     networks: {
         hardhat: {
             chainId: 31337,
@@ -44,39 +35,13 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
-        // kovan: {
-        //     url: KOVAN_RPC_URL,
-        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-        //     //accounts: {
-        //     //     mnemonic: MNEMONIC,
-        //     // },
-        //     saveDeployments: true,
-        //     chainId: 42,
-        // },
+
         rinkeby: {
             url: RINKEBY_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
+            accounts: PRIVATE_KEY,
             saveDeployments: true,
             chainId: 4,
         },
-        // mainnet: {
-        //     url: MAINNET_RPC_URL,
-        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-        //     //   accounts: {
-        //     //     mnemonic: MNEMONIC,
-        //     //   },
-        //     saveDeployments: true,
-        //     chainId: 1,
-        // },
-        // polygon: {
-        //     url: POLYGON_MAINNET_RPC_URL,
-        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-        //     saveDeployments: true,
-        //     chainId: 137,
-        // },
     },
     namedAccounts: {
         deployer: {
@@ -87,6 +52,6 @@ module.exports = {
         },
     },
     mocha: {
-        timeout: 200000,
+        timeout: 500000,
     },
 }
